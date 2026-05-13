014834deb58bd878bf53d6435c4320c0163f3286
danyavanmeyeren <261140@virtualwindow.co.za>
2026-05-13 17:15:20 +0200

    Items Added to Cart Modal

--- js/script.js
+++ js/script.js
@@ -47,3 +47,58 @@ const cartItem = document.querySelector(".cartItem");
 deleteBtn.addEventListener("click" , function (){
     cartItem.style.display = "none";
 });
+
+const addButtons = document.querySelectorAll(".addButton");
+const cartItems = document.getElementById("cartItems");
+const cartTotal = document.getElementById("cartTotal");
+
+let cart = [];
+
+addButtons.forEach(function(button){
+    button.addEventListener("click" , function () {
+        const name = button.dataset.name;
+        const price = Number(button.dataset.price);
+        const image = button.dataset.image;
+
+        cart.push({
+            name: name,
+            price: price,
+            image: image,
+            quantity: 1 
+        });
+
+        displayCart();
+    });
+});
+
+
+
+function displayCart() {
+    cartItems.innerHTML ="";
+
+    let total = 0;
+
+    cart.forEach(function (item) {
+        total =+ item.price * item.quantity;
+  cartItems.innerHTML += `
+      <div class="cartItem">
+        <img src="${item.image}" alt="${item.name}">
+
+        <div class="cartInfo">
+          <h3>${item.name}</h3>
+          <p>R ${item.price}.00</p>
+        </div>
+
+        <div class="quantityControls">
+          <button>-</button>
+          <span>${item.quantity}</span>
+          <button>+</button>
+        </div>
+
+        <button class="deleteBtn">Remove</button>
+      </div>
+    `;
+  });
+
+  cartTotal.textContent = "Total: R " + total.toFixed(2);
+}
